eaad8d9d724a3d2df50592c8b327614b8e4e5b46
ssemenidov <23sergey03@gmail.com>
2020-06-30 13:35:42 +0800

    .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.listen(3000, () => {
   console.log("listen 3000");
 });
 // app.use(express.static("./public"));
-app.get("/", (req, res) => {
+app.get("./", (req, res) => {
   res.end(`
   <h1>Hello world</h1>
   `);
