184256d60ab152d50722745afb904e9834180b4f
ssemenidov <23sergey03@gmail.com>
2020-07-01 12:56:52 +0800

    example

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const app = express();
 app.listen(3000, () => {
   console.log("listen 3000");
 });
-app.use(express.static("./public"));
+// app.use(express.static("./public"));
+app.get("/", (req, res) => {
+  res.end(`
+  <h1>Hello world</h1>
+  `);
+});
